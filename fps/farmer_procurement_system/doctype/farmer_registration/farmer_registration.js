eb1432553a03eb0f8cd785a904806a8fc77c8bd0
Madhusudan079 <madhusudan07.code@gmail.com>
2026-04-25 01:21:25 +0530

    Farmer Registration Doctype Done

---
+++ fps/farmer_procurement_system/doctype/farmer_registration/farmer_registration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Madhusudan Sharma and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Farmer Registration", {
+// 	refresh(frm) {
+
+// 	},
+// });
